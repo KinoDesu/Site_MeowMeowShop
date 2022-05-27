10961b023acf0bc770e526b0ab49d93dcf82135c
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2022-05-27 03:07:27 -0300

    added prducts in main page

--- js/script.js
+++ js/script.js
@@ -2,26 +2,30 @@
 
 var items = [
     {
-        id: 0,
+
         imagem: 'https://i.pinimg.com/564x/30/8a/20/308a20fca605c2ae0d506d502416bc63.jpg',
         nome: 'Batom rosa',
         texto: 'é um batom rosa',
         quantidade: 0
     },
     {
-        id: 1,
         imagem: 'https://i.pinimg.com/564x/77/5f/b1/775fb1ea57d34898a15829e976f325ac.jpg',
         nome: 'Peruca rosa',
         texto: 'é uma peruca rosa',
         quantidade: 0
     },
     {
-        id: 2,
         imagem: 'https://i.pinimg.com/564x/04/0b/a5/040ba5bc58207f57de9c708cb8fe93fc.jpg',
         nome: 'Peruca Loira',
         texto: 'é um peruca loira',
         quantidade: 0
-    }
+    },
+    {
+        imagem: 'https://i.pinimg.com/564x/04/0b/a5/040ba5bc58207f57de9c708cb8fe93fc.jpg',
+        nome: 'Peruca Ruiva',
+        texto: 'é um peruca Ruiva',
+        quantidade: 0
+    },
 ]
 
 inicializarLoja = () => {
@@ -31,7 +35,9 @@ inicializarLoja = () => {
                 <div class="produto-single">
                     <img src="`+ val.imagem + `"/>
                     <p>`+ val.nome + `</p>
-                    <button key="`+ val.id + `" href="#">Adicionar ao carrinho!</button>
+                    <hr>
+                    <p>`+ val.texto + `</p>
+                    <button class="add-cart" href="#"></button>
                 </div>
                 `;
     })
@@ -39,19 +45,3 @@ inicializarLoja = () => {
 }
 
 inicializarLoja();
-
-atualizarCarrinho = () => {
-    console.log(items);
-}
-
-var links = document.getElementsByTagName('button');
-
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function () {
-        let key = this.getAttribute('key');
-        items[key].quantidade++;
-        atualizarCarrinho();
-        return false;
-
-    })
-}
